a43cc3f53aa656700d9c9176d4b6c938c6bb9325
kokkiemouse <kokkiemouse@gmail.com>
2020-08-13 05:38:43 +0900

    fixed shell

--- build_auto.sh
+++ build_auto.sh
@@ -1,6 +1,2 @@
 #!/usr/bin/env zsh
-./mach build
-./mach package
-./mach build langpack-ja
-AB_CD=ja ./mach package
-./mach build installers-ja
+./mach build && ./mach package && ./mach build langpack-ja && AB_CD=ja ./mach package && ./mach build installers-ja
