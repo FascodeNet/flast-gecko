1f8de53f28c65958564611e7c38775358cbc84d0
kokkiemouse <kokkiemouse@gmail.com>
2020-08-19 15:44:01 +0900

    fixed bg
    Signed-off-by: kokkiemouse <kokkiemouse@gmail.com>

--- browser/components/newtab/data/content/newtab-render.js
+++ browser/components/newtab/data/content/newtab-render.js
@@ -4,5 +4,11 @@
 "use strict";
 
 // exported by activity-stream.bundle.js
-window.NewtabRenderUtils.renderWithoutState();
-randomBackground();
+window.NewtabRenderUtils.renderWithoutState();window.addEventListener('load', function(){
+    
+  var elementkun=document.getElementsByClassName("outer-wrapper")[0];
+  var url_head="resource://activity-stream/background-images/"
+  var n = 1+ Math.floor(Math.random() * 99);
+  var background_url=url_head + n + ".jpg";
+  elementkun.style.backgroundImage="url("  + background_url +")"; 
+});
